1a53d30e0ba972356ba1fa03ce2fde64f75293f2
Kenneth Okoro <kennethokesiena@gmail.com>
2026-01-12 17:45:59 +0100

    feat: add Joi validation for user registration and update dependencies

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const userRouter = express.Router();
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 const User = require('../models/user.model');
+
+const createUserSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    deliveryAddress: Joi.string().min(5).required()
+});
 
 userRouter.post('/', async (req, res) => {
     const { name, email, password, deliveryAddress} = req.body;
+    const joiValidation = createUserSchema.validate(req.body);
+
+    if (joiValidation.error) {
+        return res.status(400).json({ message: joiValidation.error.details[0].message });
+    }
+    
     const user = await User.findOne({ email: email});
 
     if (user) {
