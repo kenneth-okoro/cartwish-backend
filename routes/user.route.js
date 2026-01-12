7fdfc564b701c0216226512e97cafaae28d696fb
Kenneth Okoro <kennethokesiena@gmail.com>
2026-01-12 18:19:57 +0100

    feat: refactor token generation and enhance login error handling

--- routes/user.route.js
+++ routes/user.route.js
@@ -40,13 +40,36 @@ userRouter.post('/', async (req, res) => {
 
     await newUser.save();
 
-    const token = jwt.sign(
-        { _id: newUser._id, name: newUser.name},
-        process.env.JWT_KEY,
-        { expiresIn: '1h'}
-    );
+    const token = generateToken({ _id: newUser._id, name: newUser.name });
 
     res.status(201).json({ message: 'User created successfully', user: newUser, token: token });
 });
+
+userRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const token = generateToken({ _id: user._id, name: user.name })
+
+    res.status(200).json({ message: 'Login successful', user: user, token: token });
+});
+
+const generateToken = (data) => {
+    return jwt.sign(
+        data,
+        process.env.JWT_KEY,
+        { expiresIn: '1h' }
+    );
+}
 
 module.exports = userRouter;
